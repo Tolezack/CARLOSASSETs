b8b8afd84f7c4dd6b0f8ba5b30b0979a579099a3
Tolezack <tolezack33@gmail.com>
2026-09-02 20:54:15 -0400

    Add files via upload

---
+++ resenha-inferno/config.js
@@ -0,0 +1,2 @@
+window.ARENA_API_BASE = window.ARENA_API_BASE || 'https://carlos-4rxr.onrender.com';
+window.CARLOS_ASSET_BASE = window.CARLOS_ASSET_BASE || new URL('.', location.href).href.replace(/\/$/, '');
